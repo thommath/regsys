74e5e547e1df48a5cfe190dea224cdd62b008b7f
thommath <thomas-lm@vikenfiber.no>
2016-10-19 22:34:47 +0200

    minor design fix

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-<?php
-unset($_SESSION['post']);
-unset($_SESSION['from']);
-unset($_SESSION['success']);
-?>
-</section>
-</section>
-<?php if(isset($_GET['p']) && $_GET['p'] == 'regnskap'){require_once("components/regnskap/modal.php");}?>
-</body>
+      <?php
+      unset($_SESSION['post']);
+      unset($_SESSION['from']);
+      unset($_SESSION['success']);
+      ?>
+      </section>
+    </section>
+  <?php if(isset($_GET['p']) && $_GET['p'] == 'regnskap'){require_once("components/regnskap/modal.php");}?>
+  </body>
 </html>
